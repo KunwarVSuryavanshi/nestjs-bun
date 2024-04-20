692912cc5805bfbbc210a2b7c3185750b4474eb0
KunwarVSuryavanshi <virajkunwar98@gmail.com>
2024-04-21 01:23:06 +0530

    :sparkles: basic of contollers with get

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,50 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query, Redirect, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('cat')
+  greetCat(@Req() request: Request): string {
+    return this.appService.greetCat(request)
+  }
+
+  @Get('cat/breed')
+  getBreed(): string {
+    return this.appService.getBreed()
+  }
+
+  // http://localhost:3000/queryy?no=123
+  @Get('queryy')
+  getQuery(@Query('no') query): string {
+    console.log('Query', query)
+    return `Your query is ${JSON.stringify(query)}`
+  }
+
+  @Get('id/:id')
+  getParam(@Param() param): string {
+    return this.appService.getParam(param)
+  }
+
+  @Get('redirect')
+  @Redirect('https:github.com/KunwarVSuryavanshi', 301)
+  getRedirect(@Query('youtube') query) {
+    console.log('Query--->', query)
+    if (query === 'yes') {
+      return {
+        url: 'https://www.youtube.com/'
+      }
+    }
+  }
+
+  @Get('promise')
+  getPromise(): Promise<string>{
+    return this.appService.getPromise()
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ParamData } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  greetCat(request: Request): string {
+    console.log('Request--->', request.cache, request.body)
+    return 'Meow 🐈!'
+  }
+
+  getBreed(): string {
+    return 'Orange ✨'
+  }
+
+  getParam(param: ParamData): string {
+    return `Your param is ${JSON.stringify(param)}`
+  }
+
+  getPromise(): Promise<string> {
+    // for (let i = 0; i < 10000000000; i++){}
+    // setTimeout(() => {
+    // return Promise.resolve('Your Promise is resolved')
+    // }, 4000)
+    return new Promise((res) => {
+      setTimeout(() => {
+        res('Promise resolved after 4000ms')
+      }, 4000)
+    })
   }
 }
